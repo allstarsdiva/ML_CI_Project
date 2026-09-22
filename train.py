1d7fe5fa1158dd2609a20cb6e8d9c5252c2ba3a5
allstarsdiva <divakar.v09072005@gmail.com>
2026-09-22 21:52:39 +0530

    Initial ML project

---
+++ train.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+data = load_iris()
+
+X_train, X_test, y_train, y_test = train_test_split(
+    data.data,
+    data.target,
+    test_size=0.2,
+    random_state=42
+)
+
+model = LogisticRegression(max_iter=200)
+
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, predictions)
+
+print("Model Accuracy:", accuracy)
